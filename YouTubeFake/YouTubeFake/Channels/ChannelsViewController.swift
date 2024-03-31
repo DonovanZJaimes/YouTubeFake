427174b3847396adb73396ef39287e9da7bcaa8f
DonovanZJaimes <donovanzja@gmail.com>
2024-03-31 14:40:55 -0600

    Create Channels view

--- YouTubeFake/YouTubeFake/Channels/ChannelsViewController.swift
+++ YouTubeFake/YouTubeFake/Channels/ChannelsViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class ChannelsViewController: UIViewController {
 
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var imageChannel: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        infoLabel.text = "This Channel doesn't feature any other channels."
 
-        // Do any additional setup after loading the view.
+        
     }
 
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
