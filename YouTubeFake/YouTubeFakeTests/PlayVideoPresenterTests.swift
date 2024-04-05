294345e667127b320df6da22d50cc26cc481c5aa
DonovanZJaimes <donovanzja@gmail.com>
2024-04-05 13:58:16 -0600

    Add MainViewController and PlayVideoPresenter Tests

--- YouTubeFake/YouTubeFakeTests/PlayVideoPresenterTests.swift
+++ YouTubeFake/YouTubeFakeTests/PlayVideoPresenterTests.swift
@@ -6,30 +6,81 @@
 //
 
 import XCTest
+@testable import YouTubeFake
 
-final class PlayVideoPresenterTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+@MainActor class PlayVideoPresenterTests: XCTestCase {
+    var sut: PlayVideoController!
+    var delegate: PlayVideoViewMock!
+    var providerMock : PlayVideoProviderMock!
+    var videoId = "Cx2dkpBxst8"
+    var timeOut : TimeInterval = 2.0
+    
+    @MainActor override func setUpWithError() throws {
+        delegate = PlayVideoViewMock()
+        providerMock = PlayVideoProviderMock()
+        sut = PlayVideoController(delegate: delegate,provider: providerMock)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    @MainActor override func tearDownWithError() throws {
+        sut = nil
+        delegate = nil
+        providerMock = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_GetVideos () {
+        delegate.expGetRelatedVideosFinished = expectation(description: "loading video")
+        delegate.expLoading =  expectation(description: "show/hide loanding")
+        delegate.expLoading?.expectedFulfillmentCount = 2
+        Task{
+            await sut.getVideos(videoId)
         }
+        
+        waitForExpectations(timeout: timeOut) //fulfill is active or ...
+        guard let videos = sut.relatedVideoList.first as? [ItemVideo] else {
+            XCTFail("error because the desired object does not exist in the first position")
+            return
+        }
+        XCTAssertTrue(videos.first?.id == videoId)
+        XCTAssertTrue(sut.relatedVideoList.count == 2, "Videos were not found")
+        XCTAssertTrue(sut.channelModel!.id == Constants.channelIdMock, "Channel Id incorrect")
+        XCTAssertTrue(delegate.loadingViewWasCalled)
+        XCTAssertTrue(delegate.loandinHide)
+        XCTAssertTrue(delegate.loandinShow)
     }
+    
+    
 
+
+}
+class PlayVideoViewMock: PlayVideoViewProtocol {
+    var loadingViewWasCalled : Bool = false
+    var showErrorWasCalled : Bool = false
+    var getRelatedVideosFinishedWasCalled : Bool = false
+    var expGetRelatedVideosFinished : XCTestExpectation?
+    var expLoading : XCTestExpectation?
+    var expShowError: XCTestExpectation?
+    var loandinShow: Bool = false
+    var loandinHide: Bool = false
+    
+    func loadingView(_ state: YouTubeFake.LoadingViewState) {
+        if state == .show {
+            loandinShow = true
+        }else if state == .hide{
+            loandinHide = true
+        }
+        loadingViewWasCalled = true
+        expLoading?.fulfill()
+        
+        
+    }
+    
+    func showError(_ error: String, callback: (() -> Void)?) {
+        showErrorWasCalled = true
+        expShowError?.fulfill()
+    }
+    
+    func getRelatedVideosFinished() {
+        getRelatedVideosFinishedWasCalled = true
+        expGetRelatedVideosFinished?.fulfill()
+    }
 }
