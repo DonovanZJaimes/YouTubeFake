136154ec3a8991d9fe73c86b4ebf94b03c5869ef
DonovanZJaimes <donovanzja@gmail.com>
2024-04-05 14:01:00 -0600

    Modify the ChannelID of Constants

--- YouTubeFake/YouTubeFake/Utils/Constants.swift
+++ YouTubeFake/YouTubeFake/Utils/Constants.swift
@@ -8,5 +8,6 @@
 import Foundation
 struct Constants {
     static let apyKey = "AIzaSyDGiduVsaQlHSxbx3obdwf9JyjLP49p_MY"
-    static let channelId = "UC_I_lCplbwIf60iUgsTE79g"
+    static let channelIdMock = "UCQsVmhSa4X-G3lHlUtejzLA" /**Figma**/
+    static let channelId = "UCVTlvUkGslCV_h-nSAId8Sw" /**learncodeacademy**/
 }
